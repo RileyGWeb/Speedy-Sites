13d5a6a8a38b3af801e16172dfd94c71583c2ba0
Coaster132 <99291390+RileyGWeb@users.noreply.github.com>
2022-10-11 16:05:02 -0700

    Added link to calendar

--- app.js
+++ app.js
@@ -168,6 +168,7 @@ $(document).ready(function() {
 
       window.onscroll = function() {fixedTimer()};
       window.onresize = function() {fixedTimer()};
+      fixedTimer();
 
       function fixedTimer() {
         var mainContainer = document.querySelector('main');
@@ -186,7 +187,7 @@ $(document).ready(function() {
         timerBackground.style.width = mainWidth + "px";
         if (window.pageYOffset > sticky) {
           timer.classList.add("sticky");
-          document.getElementById("gift_header").style.marginTop = "calc(8rem + 75px)";
+          document.getElementById("gift_header").style.marginTop = "calc(10rem + 75px)";
           timer.style.width = mainWidth + "px";
           timer.style.marginLeft = "-" + marginLeft + "px";
           document.getElementById("splash").classList.add("sticky");
